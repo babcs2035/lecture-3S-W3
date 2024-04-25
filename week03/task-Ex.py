30d9b6a4bc0c929252235c3bf0b1dc96fa3049a7
Kosei TAKAHASHI <takahashi-kosei-34@g.ecc.u-tokyo.ac.jp>
2024-04-26 00:05:19 +0900

    [edit] week03

--- week03/task-Ex.py
+++ week03/task-Ex.py
@@ -1,102 +1,86 @@
-class SegTree:
-    def get_2pow(self, n):
-        ans = 1
-        while ans < n:
-            ans *= 2
-        return ans
+INF = 1123456789
 
-    def initialize(self):
-        start_i = self.leaf_start
-        while start_i > 1:
-            for i in range(start_i, start_i * 2, 2):
-                parent_i = i // 2
-                self.array[parent_i] = self.array[i] + self.array[i + 1]
-            start_i //= 2
 
-    def __init__(self, seq):
-        size = self.get_2pow(len(seq))
-        self.array = [0 for _ in range(2 * size)]
-        self.leaf_start = size
+class Heap:
+    def __init__(self, size):
+        self.size = size + 1
+        self.array = [INF] * self.size
+        self.last = 0
+        self.sum = 0
 
-        for i in range(self.leaf_start, self.leaf_start + len(seq)):
-            self.array[i] = seq[i - self.leaf_start]
+    def check_after_add(self, i):
+        if i < 2:
+            return
+        if self.array[i] < self.array[i // 2]:
+            self.array[i], self.array[i // 2] = self.array[i // 2], self.array[i]
+            self.check_after_add(i // 2)
 
-        self.initialize()
+    def check_after_remove(self, i):
+        if i * 2 >= self.size:
+            return
+        if self.array[i * 2] <= self.array[i * 2 + 1]:
+            if self.array[i] > self.array[i * 2]:
+                self.array[i], self.array[i * 2] = self.array[i * 2], self.array[i]
+                self.check_after_remove(i * 2)
+        else:
+            if self.array[i] > self.array[i * 2 + 1]:
+                self.array[i], self.array[i * 2 + 1] = (
+                    self.array[i * 2 + 1],
+                    self.array[i],
+                )
+                self.check_after_remove(i * 2 + 1)
 
-    def update(self, i, v):
-        node_i = i + self.leaf_start
-        self.array[node_i] = v
+    def add(self, v: int):
+        if self.last != self.size:
+            self.last += 1
+            self.sum += v
+            self.array[self.last] = v
+            self.check_after_add(self.last)
 
-        while node_i > 1:
-            parent_i = node_i // 2
-            self.array[parent_i] = (
-                self.array[parent_i * 2] + self.array[parent_i * 2 + 1]
-            )
-            node_i = parent_i
+    def remove(self):
+        if self.last != 0:
+            temp = self.array[1]
+            self.array[1] = self.array[self.last]
+            self.sum -= temp
+            self.array[self.last] = INF
+            self.last -= 1
+            self.check_after_remove(1)
+            return temp
 
-    def get_sum(self, l, r, k=1, le=0, re=-1):
-        if re == -1:
-            re = self.leaf_start
-        if re < l or r <= le:
-            return 0
-        if l <= le and re < r:
-            return self.array[k]
-        return self.get_sum(l, r, 2 * k, le, (le + re - 1) // 2) + self.get_sum(
-            l, r, 2 * k + 1, (le + re - 1) // 2 + 1, re
-        )
+    def get_size(self):
+        return self.last
+
+    def get_sum(self):
+        return self.sum
+
+    def get_min(self):
+        return self.array[1]
 
 
 Q = int(input())
-queries = []
+h_min = Heap(Q)
+h_max = Heap(Q)
 for _ in range(Q):
-    queries.append(input().split(" "))
-
-A = []
-for query in queries:
-    if len(query) == 2:
-        A.append(int(query[1]))
-
-A.sort()
-s_tree = SegTree(A)
-
-
-x = []
-a = [-1, -1, -1]
-for query in queries:
-    if len(query) == 2:
-        n = query[1]
-        if a[0] == -1:
-            a[0] = n
-        elif a[1] == -1:
-            if a[0] <= n:
-                a[1] = n
-            else:
-                a[0], a[1] = n, a[0]
-        elif a[2] == -1:
-            if n <= a[0]:
-                a[0], a[1], a[2] = n, a[0], a[1]
-            elif n <= a[1]:
-                a[1], a[2] = n, a[1]
-            else:
-                a[2] = n
-        else:
-            n
-
-
-N = len(A)
-m = N // 2
-ans = []
-for i in range(len(queries)):
-    query = queries[len(queries) - 1 - i]
-    if len(query) == 1:
-        m = tree.get_mid_val(N)
-        m_index = s_tree.find_index(m, N)
-        res = (
-            (N // 2 - 1) * m
-            - s_tree.get_sum(0, N // 2)
-            + s_tree.get_sum(N // 2, N)
-            - (N - N // 2)
+    temp = input().split(" ")
+    if len(temp) == 1:
+        m = h_max.get_min()
+        ans = (m * h_min.get_size() + h_min.get_sum()) + (
+            h_max.get_sum() - m * h_max.get_size()
         )
+        print(ans)
     else:
-        t = query[1]
-        s_tree.update(s_tree.find_index(t, N), 0)
+        v = int(temp[1])
+        if h_min.get_size() + h_max.get_size() == 0:
+            h_max.add(v)
+        elif h_min.get_size() == h_max.get_size():
+            if -h_min.get_min() <= v:
+                h_max.add(v)
+            else:
+                h_max.add(-h_min.remove())
+                h_min.add(-v)
+        else:
+            if v <= h_max.get_min():
+                h_min.add(-v)
+            else:
+                h_min.add(-h_max.remove())
+                h_max.add(v)
